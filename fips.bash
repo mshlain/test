a91a9be06588a18ffdc5f49ef28ca84c9c27d1d0
mshlain <141546361+mshlain@users.noreply.github.com>
2025-04-03 11:52:28 +0300

    ciphers

--- fips.bash
+++ fips.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "========================="
-echo "  Testing fips in kernel"
+echo "  Test fips in kernel"
 echo "========================="
 echo ""
 command_text="cat /proc/sys/crypto/fips_enabled"  # define command_text variable
@@ -41,15 +41,33 @@ echo "Result:  $result"
 echo ""
 echo ""
 echo "========================="
-echo " Print openssl providers "
+echo " Test openssl providers "
 echo "========================="
 echo ""
 command_text="openssl list -providers"
 echo "Command: $command_text"
 result=$(eval "$command_text")
 echo "Result:  $result"
+if echo "$result" | grep -q "fips" && echo "$result" | grep -q "OpenSSL FIPS Provider" && echo "$result" | grep -q "status: active"; then
+    echo -e "\e[32mFIPS provider is enabled successfully\e[0m"
+else
+    echo -e "\e[31mERROR: FIPS provider is not enabled properly\e[0m"
+fi
 
-# openssl list -providers
+echo ""
+echo ""
+echo "========================="
+echo " Test openssl ciphers   "
+echo "========================="
+echo ""
+command_text="openssl ciphers -v"
+echo "Command: $command_text"
+result=$(eval "$command_text")
+echo "Result:  $result"
 
-# echo 1 > /tmp/file
-# openssl md5 /tmp/file
+if echo "$result" | grep -q "SSLv3"; then
+    echo -e "\e[31mERROR: SSLv3 is not FIPS compliant\e[0m"
+else
+    
+    echo -e "\e[32mNo SSLv3 ciphers found, FIPS compliance is maintained\e[0m"
+fi
