92651959e53c8a424f9e8f7fc85bbe47ce4ead92
mshlain <141546361+mshlain@users.noreply.github.com>
2025-04-03 11:44:18 +0300

    continue

--- fips.bash
+++ fips.bash
@@ -1,12 +1,55 @@
 #!/bin/bash
 
-set -x
+echo "========================="
+echo "  Testing fips in kernel"
+echo "========================="
+echo ""
+command_text="cat /proc/sys/crypto/fips_enabled"  # define command_text variable
+echo "Command: $command_text"
+echo "Expected: 1"
+result=$(eval "$command_text")
+echo "Result:  $result"
+expected=1
+if [ "$result" -eq "$expected" ]; then
+    echo -e "\e[32msuccess\e[0m"
+else
+    echo -e "\e[31mERROR expected $expected got $result\e[0m"
+fi
 
-cat /proc/sys/crypto/fips_enabled
+echo ""
+echo ""
+echo "========================="
+echo " Print openssl location  "
+echo "========================="
+echo ""
+command_text="which openssl"
+echo "Command: $command_text"
+result=$(eval "$command_text")
+echo "Result:  $result"
 
-openssl version
+echo ""
+echo ""
+echo "========================="
+echo " Print openssl version  "
+echo "========================="
+echo ""
+command_text="openssl version -a"
+echo "Command: $command_text"
+result=$(eval "$command_text")
+echo "Result:  $result"
 
-openssl list -providers
+echo ""
+echo ""
+echo "========================="
+echo " Print openssl providers "
+echo "========================="
+echo ""
+command_text="openssl list -providers"
+echo "Command: $command_text"
+result=$(eval "$command_text")
+echo "Result:  $result"
 
-echo 1 > /tmp/file
-openssl md5 /tmp/file
+# openssl list -providers
+
+# echo 1 > /tmp/file
+# openssl md5 /tmp/file
