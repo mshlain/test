8198177b83011b05fbb577c50edeb1580e24a752
mshlain <141546361+mshlain@users.noreply.github.com>
2025-04-10 14:15:28 +0300

    fix

--- install/ansible.bash
+++ install/ansible.bash
@@ -35,7 +35,10 @@ fi
 UBUNTU_CODENAME="jammy"
 
 # Add the Ansible PPA signing key
-wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+rm /tmp/ansible-key.asc || true
+wget -O /tmp/ansible-key.asc "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" 
+sudo gpg --dearmour --yes -o /usr/share/keyrings/ansible-archive-keyring.gpg /tmp/ansible-key.asc
+rm /tmp/ansible-key.asc || true
 
 # Add the Ansible PPA to apt sources
 echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" > /etc/apt/sources.list.d/ansible.list
