1671a52a40302273503ec98a3e97d5c21e3d4df6
Maxim Shlain <141546361+mshlain@users.noreply.github.com>
2025-11-20 15:06:37 +0100

    Create trigger_host_command.bash

---
+++ test/trigger_host_command.bash
@@ -0,0 +1,15 @@
+TASK_ID=$(curl \
+  --unix-socket /opt/zerto/host_commands/sockets/socket \
+  -X POST "http://localhost/TasksService/StartTask" \
+  -H "Content-Type: application/json" \
+  -d '{ "CommandName":"GetBootloaderFipsConfiguration", "TimeoutSecs":"9" }' | jq -r '.Id')
+
+echo "Task ID: $TASK_ID"
+
+sleep 3
+
+curl \
+  --unix-socket /opt/zerto/host_commands/sockets/socket \
+  -X POST "http://localhost/TasksService/GetTaskInfo" \
+  -H "Content-Type: application/json" \
+  -d "{\"Id\":\"$TASK_ID\"}" | jq
